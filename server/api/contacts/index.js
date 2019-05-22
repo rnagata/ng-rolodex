dba2bc1741e67503bb9a779fb8ae052c4c20db7c
Ronald J Nagata Jr <rnagata@thetahawk.com>
2019-05-21 18:41:57 -1000

    Seventh route done

--- server/api/contacts/index.js
+++ server/api/contacts/index.js
@@ -7,12 +7,27 @@ const router = express.Router();
 router.route('/')
 .get((req, res) => {
   if (req.query.user){
-    new Contact({id: req.query.user})
-    .fetch({withRelated: 'user'})
-    .then((contact) => {
-      return res.json(contact);
+    console.log(req.query.user);
+    new Contact()
+    .where({created_by: req.query.user})
+    .fetchAll({withRelated: 'user'})
+    .then((contacts) => {
+      return res.json(contacts);
     })
   }
 });
+
+router.route('/search/:term')
+.get((req, res) => {
+  if (req.query.user){
+    new Contact()
+    .where({created_by: req.query.user})
+    .where('name', 'ilike', req.params.term.concat('%'))
+    .fetchAll({withRelated: 'user'})
+    .then((contacts) => {
+      return res.json(contacts);
+    })
+  }
+})
 
 module.exports = router;
