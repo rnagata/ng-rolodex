09c982104ee6c27f9ecacb5e16a68c17c76ba2c8
Ronald J Nagata Jr <rnagata@thetahawk.com>
2019-05-21 16:48:55 -1000

    Fifth route done

--- server/api/users/auth.js
+++ server/api/users/auth.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-
+const User = require('../../database/models/User');
 
 const router = express.Router();
 
@@ -15,6 +15,21 @@ router.route('/logout')
 .post((req, res) => {
   console.log('/logout post request');
   res.send('Logout OK');
-})
+});
+
+router.route('/register')
+.post((req, res) => {
+  console.log('/register post request, ', req.body);
+  new User()
+  .save({
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address,
+  })
+  .then(() => {
+    res.send('Register OK');
+  })
+});
 
 module.exports = router;
