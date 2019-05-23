84c8313410cad3db00f906e5c50d144b69b2beeb
Ronald J Nagata Jr <rnagata@thetahawk.com>
2019-05-22 16:18:08 -1000

    Made a Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './common_components/header/header.component';
+import { LoginForm } from './pages/login/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    LoginForm,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
